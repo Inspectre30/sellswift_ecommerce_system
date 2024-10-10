dd5553ec68a5a9eba7479f6f3307a6d0b572452a
Inspectre30 <lemuellacanlale@gmail.com>
2024-10-10 10:26:52 +0800

    new commits

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
+
+//See how does he connects the frontend login to trigger the functionalities of the backend
+//client: Login - 4:43:49
+//backend: Functionalities - 5:50:20
