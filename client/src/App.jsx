3663964faeaf72c120c4d06e9e1556ab141512fd
Inspectre30 <lemuellacanlale@gmail.com>
2024-10-11 16:38:41 +0800

    new commits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,9 +9,11 @@ import Login from './pages/Login'
 import Cart from './pages/Cart'
 import Product from './pages/Product'
 import About from './pages/About'
+import Navbar from "./components/Navbar";
 const App = () => {
   return(
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/collection" element={<Collection />} />
