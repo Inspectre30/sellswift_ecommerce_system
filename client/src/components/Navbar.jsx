c74749c1e3d638e683bdc40e62e9a77d3fa8813b
Inspectre30 <lemuellacanlale@gmail.com>
2024-10-18 18:03:03 +0800

    new commits

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/assets/frontend_assets/assets";
 import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const {setShowSearch} = useContext(ShopContext);
+  const {setShowSearch,getCartCount} = useContext(ShopContext);
   return (
     <div className="flex items-center justify-between py-5">
       <Link to="/">
@@ -57,7 +57,7 @@ const Navbar = () => {
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-red-700 text-white aspect-square rounded-full text-[8px]">
-            10
+           {getCartCount()}
           </p>
         </Link>
         <img
--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -2,12 +2,14 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets/frontend_assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
+  const [size, setSize] = useState("")
   const fetchProductData = async () => {
     products.map((item) => {
       if (item._id === productId) {
@@ -57,8 +59,38 @@ const Product = () => {
           </div>
           <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
           <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index)=>(
+                <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500':''}`} key={index}>{item}</button>
+              ))}
+            </div>
+          </div>
+          <button onClick={()=> addToCart(productData._id, size)}className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
+          <hr className="mt-8 sm:w-4/5"/>
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+                <p>100% Original Product</p>
+                <p>Cash on delivery is available on this product.</p>
+                <p></p>
+          </div>
         </div>
       </div>
+
+      {/* Description and review section  */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.</p>
+        </div>
+      </div>
+
+
+      {/* Display related products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
     </div>
   ) : (
     <div className="opacity-0"></div>
@@ -66,4 +98,4 @@ const Product = () => {
 };
 
 export default Product;
-//3:02:38
+//3:04:18
