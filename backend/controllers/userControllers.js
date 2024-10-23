a40bd27a5f5ccfe9bba21c32d34a3e41c819ec53
Inspectre30 <lemuellacanlale@gmail.com>
2024-10-23 15:55:58 +0800

    new commits

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,17 +1,82 @@
+import validator from "validator";
+import userModel from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 //Route for user login
-const loginUser = async (req,res) => {
+const loginUser = async (req, res) => {
+    try{
+        const{email, password} = req.body;
 
-}
+        const user = await userModel.findOne({email});
+
+        if(!user) {
+            return res.json({success:false, message:"User doesn't exists"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password); 
+
+        if(isMatch) {
+            const token = createToken(user._id)
+            res.json({success:true, token})
+        }
+        else{
+            res.json({success: false, msg:"Invalid Password"})
+        }
+    }catch(err) {
+            console.log(err); 
+            res.json({success: false, msg: err.message})
+    }
+};
 //route for register
 const registerUser = async (req, res) => {
-    res.json({msg:'Register API Working :>'})
+  try {
+    const { name, email, password } = req.body;
+    //checking user already exists or not
+    const exist = await userModel.findOne({ email });
+    if (exist) {
+      return res.json({ success: false, msg: "User already exist" });
+    }
+    //validating email and strong password
 
-}
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        msg: "Please use a valid email address",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        msg: "Please enter a strong password",
+      });
+    }
+    //hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (err) {
+    console.log(err);
+    res.json({success: false, message: err.message})
+  }
+};
 //route for seller
-const sellerLogin = async(req, res) => {
+const sellerLogin = async (req, res) => {};
 
-}
+export { loginUser, registerUser, sellerLogin };
 
 
-export{loginUser,registerUser,sellerLogin}
