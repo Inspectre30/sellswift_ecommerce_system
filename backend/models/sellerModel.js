9b36c9bac19f1c804184f2262fa638480f3a876c
Inspectre30 <lemuellacanlale@gmail.com>
2025-01-05 12:13:02 -0800

    new commits

--- backend/models/sellerModel.js
+++ backend/models/sellerModel.js
@@ -2,14 +2,22 @@ import mongoose from "mongoose";
 
 const sellerSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    verifyOtp: { type: String, default: "" },
+    verifyOtpExpireAt: { type: Number, default: 0 },
+    isAccountVerified: { type: Boolean, default: false },
+    resetOtp: { type: String, default: "" },
+    resetOtpExpireAt: { type: Number, default: 0 },
     storeName: { type: String, required: true },
     storeDescription: { type: String },
     storeAddress: { type: String },
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "product" }],
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "product", default: {}}],
+    status: { type: String, enum: ["active", "banned"], default: "active" },
   },
   { timestamps: true }
 );
 
-const sellerModel = mongoose.models.seller || mongoose.model("seller", sellerSchema);
+const sellerModel =
+mongoose.models.seller || mongoose.model("seller", sellerSchema);
 export default sellerModel;
