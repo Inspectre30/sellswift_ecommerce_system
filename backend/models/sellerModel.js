f04586e03655191e75029a85796671873170d53a
Inspectre <116529425+Inspectre30@users.noreply.github.com>
2025-01-07 18:45:38 -0800

    Update sellerModel.js

--- backend/models/sellerModel.js
+++ backend/models/sellerModel.js
@@ -20,11 +20,7 @@ const sellerSchema = new mongoose.Schema(
         default: [],
       },
     ],
-    inventory_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "inventory", 
-      required: true,
-    },
+
     status: { type: String, enum: ["active", "banned"], default: "active" },
     permit: {
       documentUrl: { type: String, required: false }, // URL of the uploaded permit document
@@ -43,36 +39,3 @@ export default sellerModel;
 
 
 
-// import mongoose from "mongoose";
-
-// const sellerSchema = new mongoose.Schema(
-//   {
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-//     verifyOtp: { type: String, default: "" },
-//     phone: {type:String},
-//     verifyOtpExpireAt: { type: Number, default: 0 },
-//     isEmailAccountVerified: { type: Boolean, default: false },
-//     resetOtp: { type: String, default: "" },
-//     resetOtpExpireAt: { type: Number, default: 0 },
-//     storeName: { type: String, required: true },
-//     storeDescription: { type: String },
-//     storeAddress: { type: String },
-  
-//     status: { type: String, enum: ["active", "banned"], default: "active" },
-//     permit: {
-//       documentUrl: { type: String, required: false }, // URL of the uploaded permit document (e.g., stored in Cloudinary or other storage service)
-//       status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" }, // Current status of the permit
-//       submittedAt: { type: Date }, // Date the permit was submitted
-//       reviewedAt: { type: Date }, // Date the permit was reviewed
-//       reviewedBy: { type: mongoose.Schema.Types.ObjectId, ref: "admin" }, // Admin who reviewed the permit
-//       rejectionReason: { type: String, default: "" }, // Reason for rejection, if applicable
-//     },
-    
-//   },
-//   { timestamps: true }
-// );
-
-// const sellerModel =
-// mongoose.models.seller || mongoose.model("seller", sellerSchema);
-// export default sellerModel;
